99e93dd04d471ffeb71d0c4b5f510487f7d317ee
Abby Holland <abigailholland22@gmail.com>
2020-01-27 13:53:05 -0500

    updated team info

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -12,11 +12,11 @@ import cam from '../assets/headshots/cam.jpg';
 import mike from '../assets/headshots/mike.jpg';
 import mo from '../assets/headshots/mo.jpg';
 import theo from '../assets/headshots/theo.jpg';
-//import awni from '../assets/headshots/awnijpg';
-//import josh from '../assets/headshots/josh.jpg';
-//import derek from '../assets/headshots/derek.jpg';
-//import nate from '../assets/headshots/nate.jpg';
-//import umer from '../assets/headshots/umer.jpg';
+import awni from '../assets/headshots/awni.jpg';
+import josh from '../assets/headshots/josh.jpg';
+import derek from '../assets/headshots/derek.jpg';
+import nate from '../assets/headshots/nate.jpg';
+import umer from '../assets/headshots/umer.jpg';
 
 
 
@@ -82,6 +82,87 @@ function Team() {
                       <img src={cam} className="img-fluid" />
                       <h3>Cam McKay</h3>
                       <p>Backend Development Lead</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className="team-members">
+              <div className="container">
+                <div className="row">
+                  <div className="col-sm-4">
+                    <div className="team-member">
+                      <img src={mike} className="img-fluid" />
+                      <h3>Mike Kurdyla</h3>
+                      <p>Backend Developer</p>
+                    </div>
+                  </div>
+                  <div className="col-sm-4">
+                    <div className="team-member">
+                      <img src={mo} className="img-fluid" />
+                      <h3>Mo </h3>
+                      <p>Backend Developer</p>
+                    </div>
+                  </div>
+                  <div className="col-sm-4">
+                    <div className="team-member">
+                      <img src={theo} className="img-fluid" />
+                      <h3>Theo </h3>
+                      <p>Backend Developer</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className="team-members">
+              <div className="container">
+                <div className="row">
+                  <div className="col-sm-4">
+                    <div className="team-member">
+                      <img src={josh} className="img-fluid" />
+                      <h3>Josh Kanattu</h3>
+                      <p>Backend Developer</p>
+                    </div>
+                  </div>
+                  <div className="col-sm-4">
+                    <div className="team-member">
+                      <img src={derek} className="img-fluid" />
+                      <h3>Derek Zhang </h3>
+                      <p>Backend Developer</p>
+                    </div>
+                  </div>
+                  <div className="col-sm-4">
+                    <div className="team-member">
+                      <img src={theo} className="img-fluid" />
+                      <h3>Theo </h3>
+                      <p>Backend Developer</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className="team-members">
+              <div className="container">
+                <div className="row">
+                  <div className="col-sm-4">
+                    <div className="team-member">
+                      <img src={nate} className="img-fluid" />
+                      <h3>Nate Nethercott</h3>
+                      <p>Artificial Intelligence Developer</p>
+                    </div>
+                  </div>
+                  <div className="col-sm-4">
+                    <div className="team-member">
+                      <img src={awni} className="img-fluid" />
+                      <h3>Awni </h3>
+                      <p>Artificial Intelligence Developer</p>
+                    </div>
+                  </div>
+                  <div className="col-sm-4">
+                    <div className="team-member">
+                      <img src={umer} className="img-fluid" />
+                      <h3>Umer Kamran</h3>
+                      <p>Artificial Intelligence Developer</p>
                     </div>
                   </div>
                 </div>
