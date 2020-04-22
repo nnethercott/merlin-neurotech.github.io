69033e1ed7ccb1978baf47405f4fa5d7a85ca88b
Abby Holland <abigailholland22@gmail.com>
2020-04-22 14:11:57 -0400

    Updates

---
+++ precache-manifest.02d6d4a9929c873293da4a3e917d7547.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed72db7e3bbea95d4603aaddd0ae8f78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d06b82b5c6f8b28852fb",
+    "url": "/static/css/main.88a47159.chunk.css"
+  },
+  {
+    "revision": "9e25b4cdb4494a928619",
+    "url": "/static/js/2.c7646ee5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c7646ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d06b82b5c6f8b28852fb",
+    "url": "/static/js/main.1756d38d.chunk.js"
+  },
+  {
+    "revision": "5380f38e122b92afd283",
+    "url": "/static/js/runtime-main.d9330612.js"
+  },
+  {
+    "revision": "39ba8f40ea2d7dbf32c7dfb9752ffcdd",
+    "url": "/static/media/Logo.39ba8f40.svg"
+  },
+  {
+    "revision": "318ae734ddd6d21a38d8eeb5e31b3d1c",
+    "url": "/static/media/abby.318ae734.jpg"
+  },
+  {
+    "revision": "3f489956d897344c5481cd72ac87e258",
+    "url": "/static/media/awni.3f489956.jpg"
+  },
+  {
+    "revision": "c72af2fb9ee9a3be9992a5d41674efb5",
+    "url": "/static/media/brain-min.c72af2fb.PNG"
+  },
+  {
+    "revision": "064eee95c923b7414953ada7ac0a96ce",
+    "url": "/static/media/cam.064eee95.jpg"
+  },
+  {
+    "revision": "4963a593ee284b8006c36a2723ac3b72",
+    "url": "/static/media/cns.4963a593.png"
+  },
+  {
+    "revision": "78e4f57f37a597a40c7204881ec8536b",
+    "url": "/static/media/derek.78e4f57f.jpg"
+  },
+  {
+    "revision": "b48da7425c3503004f18fa97283904a5",
+    "url": "/static/media/edu.b48da742.jpg"
+  },
+  {
+    "revision": "a1a1234da200603396391789027e12a0",
+    "url": "/static/media/group.a1a1234d.svg"
+  },
+  {
+    "revision": "1ef765c2ba54a0af6cc0c8a28cd93498",
+    "url": "/static/media/josh.1ef765c2.jpg"
+  },
+  {
+    "revision": "8de0a2d53c8bc039c44bdfc6084c33aa",
+    "url": "/static/media/julia.8de0a2d5.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "37f09984b321dd67fca0e5e04326fc3c",
+    "url": "/static/media/lukas.37f09984.jpg"
+  },
+  {
+    "revision": "bf6553204f1e3282cee8839cf2bb29db",
+    "url": "/static/media/mike.bf655320.jpg"
+  },
+  {
+    "revision": "3f830b365cb95db106c27c0121850d04",
+    "url": "/static/media/mo.3f830b36.jpg"
+  },
+  {
+    "revision": "26b56e7abad822009df06399fe3a5521",
+    "url": "/static/media/monetization.26b56e7a.svg"
+  },
+  {
+    "revision": "f1dd853c7c1620091dccc69121cef03d",
+    "url": "/static/media/nate.f1dd853c.jpg"
+  },
+  {
+    "revision": "eda2c6e4c9aed4e4b7d2d5ae4fd02f88",
+    "url": "/static/media/openbci.eda2c6e4.png"
+  },
+  {
+    "revision": "66f12fbefcb305aaa255f84feb3fafc6",
+    "url": "/static/media/running.66f12fbe.svg"
+  },
+  {
+    "revision": "be0fbc17e7861d278b6a5437a76fc7e4",
+    "url": "/static/media/sam.be0fbc17.jpg"
+  },
+  {
+    "revision": "b11f78d68b4e91fd7376dbf661470b73",
+    "url": "/static/media/school.b11f78d6.svg"
+  },
+  {
+    "revision": "a919461fbf1575b37c694cb73ab4bd2c",
+    "url": "/static/media/shawn.a919461f.jpg"
+  },
+  {
+    "revision": "a6cb2a5853f90199717ffda7a6a70e4b",
+    "url": "/static/media/smart-watch.a6cb2a58.jpg"
+  },
+  {
+    "revision": "be55a7e288ef98af24f3eb4b1d66c20b",
+    "url": "/static/media/stock1.be55a7e2.jpg"
+  },
+  {
+    "revision": "c1ae74ad01b083b4ee8b92d6983b97ce",
+    "url": "/static/media/team.c1ae74ad.jpg"
+  },
+  {
+    "revision": "94c23d78947056bf8e8387f33359bc74",
+    "url": "/static/media/textandlogo.94c23d78.svg"
+  },
+  {
+    "revision": "5b7a167b0d8e46c80a916c68e417627c",
+    "url": "/static/media/theo.5b7a167b.jpg"
+  },
+  {
+    "revision": "c3d4adae5b99b2fc243450b3f47463d8",
+    "url": "/static/media/umer.c3d4adae.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/merlin-neurotech/precache-manifest.0a730eb943f675694ec5b5d1ffddd4c4.js"
+  "/precache-manifest.02d6d4a9929c873293da4a3e917d7547.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/merlin-neurotech/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
